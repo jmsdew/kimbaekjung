d5dbf2e14b04833aa81eeb42a78c2e5e8140d1be
kiyu <giu0428@gmail.com>
2024-01-03 12:13:16 +0900

    admin 로그인 업데이트

--- semi-project/src/main/java/com/kimbaekjung/semiproject/admin/controller/LoginController.java
+++ semi-project/src/main/java/com/kimbaekjung/semiproject/admin/controller/LoginController.java
@@ -1,14 +1,24 @@
 package com.kimbaekjung.semiproject.admin.controller;
 
 import com.kimbaekjung.semiproject.admin.dto.LoginDTO;
+import com.kimbaekjung.semiproject.admin.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
 
     @GetMapping("/adminLoginPage")
     public String adminlogin() {
@@ -21,7 +31,17 @@ public class LoginController {
     }
 
     @PostMapping("/adminLogin")
-    public String login(LoginDTO loginDTO) {
-        return "/admin/admin_home";
+    public ModelAndView login(ModelAndView mv, @RequestParam String adminId, @RequestParam String adminPass) {
+
+        LoginDTO selectedAdmin = loginService.adminLogin(adminId);
+
+        if (selectedAdmin != null && Objects.equals(selectedAdmin.getPass(), adminPass)) {
+            mv.setViewName("/admin/admin_home");
+        } else {
+            mv.addObject("message", "아이디나 비밀번호가 틀렸습니다.");
+            mv.setViewName("/admin/admin_login");
+        }
+        return mv;
+
     }
 }
